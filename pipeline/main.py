8a107e8158a5d42da977db2813123c824da26adf
stephentea <shchien@andrew.cmu.edu>
2025-11-23 04:46:15 +0000

    results

--- pipeline/main.py
+++ pipeline/main.py
@@ -39,16 +39,21 @@ def main():
                 x, y = np.array(x), np.array(y)
                 segments = segment_trace_pelt(x, y)
                 curr_segments.append(segments)
-                plot_segmented_trace(x, y, segments, filename=f'{client.name}')
+                plot_segmented_trace(x, y, segments, filename=f'{experiment.name}-{client.name}')
             all_segments.append(curr_segments)
         
         # Analyze TTLB
         single_ttlb_res, pairwise_ttlb_res = analyze_ttlb(analysis)
-        plot_ttlb_barchart(single_ttlb_res, filename=f'ttlb-barchart-{client.name}')
-        plot_ttlb_heatmap(pairwise_ttlb_res, filename=f'ttlb-heatmap-{client.name}')
+        plot_ttlb_barchart(single_ttlb_res, filename=f'ttlb-barchart-{experiment.name}')
+        plot_ttlb_heatmap(pairwise_ttlb_res, filename=f'ttlb-heatmap-{experiment.name}')
 
         # Analyze segments
-        segment_cmp = segment_analysis(all_segments[0], all_segments[1], "proxygen", "ngtcp2")
-        print(find_first_significant_difference(segment_cmp))
-        visualize_segment_comparison(segment_cmp)
+        segment_cmp = segment_analysis(all_segments[0], all_segments[1], "proxygen_h3", "ngtcp2_h3")
+        visualize_segment_comparison(segment_cmp, filename=f'segments-{experiment.name}-1')
+
+        segment_cmp = segment_analysis(all_segments[0], all_segments[2], "proxygen_h3", "curl_h2")
+        visualize_segment_comparison(segment_cmp, filename=f'segments-{experiment.name}-2')
+
+        segment_cmp = segment_analysis(all_segments[1], all_segments[2], "ngtcp2_h3", "curl_h2")
+        visualize_segment_comparison(segment_cmp, filename=f'segments-{experiment.name}-3')
 main()
